27d81889eb5540dcdc9d9621510b1fb351d62796
TheNotary <no@email.plz>
2016-05-05 12:23:42 -0700

    using configs to decide where to pull fresh files from

--- dokku_proxy/lib/dokku_proxy.js
+++ dokku_proxy/lib/dokku_proxy.js
@@ -1,5 +1,9 @@
 // Configs
-var port = 8000;
+var port = 8000,
+    controllerToDomainDic = {
+      "/heroku-buildpack-ruby": "https://s3-external-1.amazonaws.com/",
+      "/node": "https://s3pository.heroku.com/"
+    };
 
 // Dependencies
 var path = require("path"),
@@ -39,12 +43,7 @@ var server = http.createServer(function(req, response) {
     on('error', function(err) {
       if (err.code === 'ENOENT') {
 
-        // Determine where to pull the file from
-        var originDomain;
-        if (req.url.lastIndexOf("/heroku-buildpack-ruby", 0) === 0)
-          originDomain = "https://s3-external-1.amazonaws.com/";
-        else if (req.url.lastIndexOf("/node", 0) === 0)
-          originDomain = "https://s3pository.heroku.com/";
+        var originDomain = pickDomainToPullFrom(req.url);
 
         var request = https.get(originDomain + req.url).on('response', function(originResponse) {
           response.writeHead(200, {
@@ -71,3 +70,15 @@ var server = http.createServer(function(req, response) {
 
 console.log("listening on port " + port);
 server.listen(port);
+
+
+// Looks up what directory the file is being requested from.  This signals us
+// what domain has the original file
+function pickDomainToPullFrom(url) {
+  var result;
+  Object.keys(controllerToDomainDic).forEach(function(key) {
+    if (url.lastIndexOf(key, 0) === 0)
+      result = controllerToDomainDic[key];
+  });
+  return result;
+}
